c1153ac82dd3dbc6c0d8380daa5b04c8ccf8bfb2
pro-utkarshM <projects.utkarshMaurya@gmail.com>
2024-06-13 11:28:25 +0530

    added: ref

--- scripts/lfs.sh
+++ scripts/lfs.sh
@@ -2,7 +2,7 @@
 
 export LFS=/mnt/lfs
 export LFS_TGT=x86_64-lfs-linux-gnu
-export LFS_DISK=/dev/sdb
+export LFS_DISK=/dev/sda
 
 if ! grep -q "$LFS" /proc/mounts; then
   source setupdisk.sh "$LFS_DISK"
